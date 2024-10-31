5446af499734d0c51c9c285085ba68b7f0ffa4d4
YursKsf1 <yurs.ksf@gmail.com>
2024-10-30 19:40:09 -0500

    operaciones logicas

---
+++ clase3/operaciones_logicas.js
@@ -0,0 +1,33 @@
+// Igualdad
+
+8 == 8 // true
+8 != 9 // true
+8 != 8 // false
+
+// mayor o menor > <
+5>3 // true
+5>5 // false
+5>=5 // true
+15>5 // true
+15>115 // false
+
+6<12 // true
+16<12 // false
+16<=16 // true
+
+/*OPERACIONES LOGICAS
+Negación (!): Invierte el valor de verdad de una proposición.
+
+Conjunción (&&): Verdadero si ambas proposiciones son verdaderas.
+
+Disyunción (||): Verdadero si al menos una proposición es verdadera.
+*/
+
+// Negaciones
+
+!false //true
+!true //false
+
+// Conjunción (&&) y Disyunción (||)
+true && false // false
+true || false // true
