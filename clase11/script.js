d629860fa69024f3bd64e8a56c6fec35ad343c5f
YursKsf1 <yurs.ksf@gmail.com>
2024-11-29 20:23:10 -0500

    ejercicio task

---
+++ clase11/script.js
@@ -0,0 +1,30 @@
+window.onload = (e) => {
+// renderizar tareas
+// agregar escucha al boton de guardar
+}
+
+tareas = [
+    {
+        "nombre": "tender la cama",
+        "prioridad": 5
+    },
+    {
+        "nombre": "preparar el desayuno",
+        "prioridad": 9
+    },
+    {
+        "nombre": "limpar la cocina",
+        "prioridad": 7
+    },
+    {
+        "nombre": "sacar a pasear el perro",
+        "prioridad": 9
+    },
+    {
+        "nombre": "lavar la ropa",
+        "prioridad": 3
+    },
+    // agregar mas tareas con su respectiva prioridad
+]
+
+// definir funciones de filtrado y renderizado 
